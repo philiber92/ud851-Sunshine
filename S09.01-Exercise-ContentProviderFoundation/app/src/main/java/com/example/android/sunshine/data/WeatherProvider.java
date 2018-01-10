770c01ed37a6525ddc76914f7c37b78a1ddea641
Unknown <P.Bergt@gmx.de>
2018-01-10 02:32:56 +0100

    solved S09-01

--- S09.01-Exercise-ContentProviderFoundation/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ S09.01-Exercise-ContentProviderFoundation/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -18,9 +18,14 @@ package com.example.android.sunshine.data;
 import android.annotation.TargetApi;
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
+
+import static com.example.android.sunshine.data.WeatherContract.*;
+import static com.example.android.sunshine.data.WeatherContract.WeatherEntry.*;
 
 /**
  * This class serves as the ContentProvider for all of Sunshine's data. This class allows us to
@@ -34,21 +39,27 @@ import android.support.annotation.NonNull;
  */
 public class WeatherProvider extends ContentProvider {
 
-//  TODO (5) Create static constant integer values named CODE_WEATHER & CODE_WEATHER_WITH_DATE to identify the URIs this ContentProvider can handle
+    private static final int CODE_WEATHER = 100;
+    private static final int CODE_WEATHER_WITH_DATE = 101;
 
-//  TODO (7) Instantiate a static UriMatcher using the buildUriMatcher method
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
 
     WeatherDbHelper mOpenHelper;
 
-//  TODO (6) Write a method called buildUriMatcher where you match URI's to their numeric ID
+    public static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
-//  TODO (1) Implement onCreate
+        uriMatcher.addURI(CONTENT_AUTHORITY, PATH_WEATHER, CODE_WEATHER);
+        uriMatcher.addURI(CONTENT_AUTHORITY, String.format("%s/#", PATH_WEATHER), CODE_WEATHER_WITH_DATE);
+
+        return uriMatcher;
+    }
+
     @Override
     public boolean onCreate() {
-//      TODO (2) Within onCreate, instantiate our mOpenHelper
+        mOpenHelper = new WeatherDbHelper(getContext());
 
-//      TODO (3) Return true from onCreate to signify success performing setup
-        return false;
+        return true;
     }
 
     /**
@@ -69,7 +80,6 @@ public class WeatherProvider extends ContentProvider {
         throw new RuntimeException("Student, you need to implement the bulkInsert mehtod!");
     }
 
-//  TODO (8) Provide an implementation for the query method
     /**
      * Handles query requests from clients. We will use this method in Sunshine to query for all
      * of our weather data as well as to query for the weather on a particular day.
@@ -88,11 +98,42 @@ public class WeatherProvider extends ContentProvider {
     @Override
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        throw new RuntimeException("Student, implement the query method!");
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
 
-//      TODO (9) Handle queries on both the weather and weather with date URI
+        Cursor resultCursor;
 
-//      TODO (10) Call setNotificationUri on the cursor and then return the cursor
+        switch (sUriMatcher.match(uri)) {
+            case CODE_WEATHER:
+                resultCursor = db.query(
+                        TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            case CODE_WEATHER_WITH_DATE:
+                String date = uri.getPathSegments().get(1);
+                resultCursor = db.query(
+                        TABLE_NAME,
+                        projection,
+                        "?=?",
+                        new String[] {COLUMN_DATE, date},
+                        null,
+                        null, sortOrder
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unsupported Operation!");
+        }
+
+        if(resultCursor != null) {
+            resultCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        }
+
+        return resultCursor;
     }
 
     /**
