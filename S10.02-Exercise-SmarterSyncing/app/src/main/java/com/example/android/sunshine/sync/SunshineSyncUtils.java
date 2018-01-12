a0b221e10459a8925ab3c82b2fcd02e1f8bb6763
Unknown <P.Bergt@gmx.de>
2018-01-12 07:36:51 +0100

    solved S10-02

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,20 +15,43 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
+import com.example.android.sunshine.data.WeatherProvider;
+
+import static com.example.android.sunshine.data.WeatherContract.WeatherEntry.*;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized = false;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    public static void initialize(Context context) {
+        if(sInitialized) return;
+
+        ContentResolver contentResolver = context.getContentResolver();
+
+        Cursor cursor = contentResolver.query(
+                CONTENT_URI,
+                null ,
+                null,
+                null ,
+                null
+        );
+
+        if(cursor == null || cursor.getCount() == 0) {
+            startImmediateSync(context);
+        }
+
+        sInitialized = true;
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
